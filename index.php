2fa89e633b67e9ce2bc2725df0dcbc9ddfc48e06
Siarhei Vauchok <byqdes@gmail.com>
2018-08-23 20:21:12 +0300

    new branch?

--- index.php
+++ index.php
@@ -6,4 +6,22 @@ echo "test";
 //another comment 
 // comments, comments
 
+//new branch test
+
+class some_class{
+
+	public function echo($message){
+		echo $message;
+	}
+
+}
+
+
+class another_class extends some_class{
+
+}
+
+$obj = new another_class();
+$obj->echo('test');
+
 ?>
